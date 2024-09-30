1c366d96628f19d9faddd34e387cc5becf8b0498
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-30 10:15:08 -0400

    Modify the function to also user prompt message

--- chatboy.py
+++ chatboy.py
@@ -10,10 +10,10 @@ otherwise tell them invalid number
 """
 
 
-def validate_int_input(user_response: str) -> int:
+def validate_int_input(user_response: str, user_input_message: str) -> int:
     while not user_response.isdigit():
         print(f"{user_response} is not a number")
-        user_response: str = input("> User: ")
+        user_response: str = input(user_input_message)
 
     return user_response
 
@@ -25,24 +25,35 @@ print(
 
 user_input: str = input("> User: ")
 
-user_input = validate_int_input(user_input)
+user_input = validate_int_input(user_input, "\n> User: ")
 
 if user_input == "1":
-    first_number: str = input("Enter the first number")
-    first_number = validate_int_input(first_number)
+    first_number: str = input("Enter the first number\n> User: ")
+    first_number = validate_int_input(first_number, "\n> User: ")
 
-    second_number: str = input("Enter the second number")
-    second_number = validate_int_input(second_number)
-
+    second_number: str = input("Enter the second number\n> User: ")
+    second_number = validate_int_input(
+        second_number, "Enter the second number\n> User: "
+    )
 
 elif user_input == "2":
     first_number: str = input("Enter the first number")
+    first_number = validate_int_input(first_number, "Enter the first number\n> User: ")
+
     second_number: str = input("Enter the second number")
+    second_number = validate_int_input(
+        second_number, "Enter the second number\n> User: "
+    )
 
 
 elif user_input == "3":
     first_number: str = input("Enter the first number")
+    first_number = validate_int_input(first_number, "Enter the first number\n> User: ")
+
     second_number: str = input("Enter the second number")
+    second_number = validate_int_input(
+        second_number, "Enter the second number\n> User: "
+    )
 
 else:
     print(f"{user_input} is not a valid input option")
