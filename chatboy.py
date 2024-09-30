20deb537ef7c6d0f0c0415f1e6ab4044be720e28
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-30 09:32:55 -0400

    Write a function to validate integer response from users

--- chatboy.py
+++ chatboy.py
@@ -9,6 +9,15 @@ otherwise tell them invalid number
 
 """
 
+
+def validate_int_input(user_response: str) -> int:
+    while not user_response.isdigit():
+        print(f"{user_response} is not a number")
+        user_response: str = input("> User: ")
+
+    return user_response
+
+
 print(
     "Hello. What would you like to do today?\n1. Add two numbers.\n2. Subtract two numbers\n3. Multiple two numbers"
 )
@@ -16,6 +25,24 @@ print(
 
 user_input: str = input("> User: ")
 
-while not user_input.isdigit():
-    print(f"{user_input} is not a number")
-    user_input: str = input("> User: ")
+user_input = validate_int_input(user_input)
+
+if user_input == "1":
+    first_number: str = input("Enter the first number")
+    first_number = validate_int_input(first_number)
+
+    second_number: str = input("Enter the second number")
+    second_number = validate_int_input(second_number)
+
+
+elif user_input == "2":
+    first_number: str = input("Enter the first number")
+    second_number: str = input("Enter the second number")
+
+
+elif user_input == "3":
+    first_number: str = input("Enter the first number")
+    second_number: str = input("Enter the second number")
+
+else:
+    print(f"{user_input} is not a valid input option")
