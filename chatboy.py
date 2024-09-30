ccf737567c206cf684a9cacda4b83eac0f443494
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-30 10:30:12 -0400

    Add a while loop to ensure user enters a valid option between 1-3

--- chatboy.py
+++ chatboy.py
@@ -12,7 +12,7 @@ otherwise tell them invalid number
 user_prompt: str = "\n> User: "
 
 
-def validate_int_input(user_response: str, user_input_message: str) -> int:
+def validate_int_input(user_response: str, user_input_message: str) -> str:
     while not user_response.isdigit():
         print(f"{user_response} is not a number")
         user_response: str = input(user_input_message)
@@ -27,7 +27,13 @@ print(
 
 user_input: str = input(user_prompt)
 
-user_input = validate_int_input(user_input, f"{user_prompt} ")
+user_input = validate_int_input(user_input, f"{user_prompt}")
+
+while (user_input != "1") and (user_input != "2") and (user_input != "3"):
+    print(f"{user_input} is not a valid input option")
+    user_input = input(
+        f"What would you like to do today?\n1. Add two numbers.\n2. Subtract two numbers\n3. Multiple two numbers{user_prompt}"
+    )
 
 if user_input == "1":
     first_number: str = input(f"Enter the first number{user_prompt} ")
@@ -49,7 +55,6 @@ elif user_input == "2":
         second_number, f"Enter the second number{user_prompt} "
     )
 
-
 elif user_input == "3":
     first_number: str = input(f"Enter the first number{user_prompt}")
     first_number = validate_int_input(
@@ -60,6 +65,3 @@ elif user_input == "3":
     second_number = validate_int_input(
         second_number, f"Enter the second number{user_prompt} "
     )
-
-else:
-    print(f"{user_input} is not a valid input option")
