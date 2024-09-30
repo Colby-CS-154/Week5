048c9478208f19c92940a9911b49f6f9302305e0
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-30 09:15:35 -0400

    Print menu option

---
+++ chatboy.py
@@ -0,0 +1,14 @@
+"""
+Get input from the user
+
+Ensure that the input is a number
+If the number is 1, add them
+If the number is 2, subtract them
+If the number is 3, multiple them
+otherwise tell them invalid number
+
+"""
+
+print(
+    "Hello. What would you like to do today?\n1. Add two numbers.\n2. Subtract two numbers\n3. Multiple two numbers"
+)
